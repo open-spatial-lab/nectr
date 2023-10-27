a7769e04eb2566f281f8951faec9ffe1abbf4fdf
nofurtherinformation <dhalpern@uchicago.edu>
2023-10-27 08:24:47 -0500

    cleanup api app deploy

--- apps/api/webiny.application.ts
+++ apps/api/webiny.application.ts
@@ -1,11 +1,10 @@
 import { createApiApp } from '@webiny/serverless-cms-aws'
-import { addAccountIdToEnv } from './data/src/infra/injectPermissions'
 import { reallyHackyPluginAfterBuild } from './webiny.aferBuild.hackyFix'
 import { createDataApp } from './data/src/infra/createDataApp'
 
 export default createApiApp({
   pulumiResourceNamePrefix: 'wby-',
-  plugins: [addAccountIdToEnv(), reallyHackyPluginAfterBuild()],
+  plugins: [reallyHackyPluginAfterBuild()],
   pulumi(app) {
 
     const dataApp = createDataApp(app)
